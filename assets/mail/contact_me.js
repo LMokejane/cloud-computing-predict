30be98b251c215f86ff7055d038a08b18b02dc57
LMokejane <168183746+LMokejane@users.noreply.github.com>
2024-08-27 15:56:40 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://5x6c1mmuu9.execute-api.eu-west-1.amazonaws.com/dev",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
